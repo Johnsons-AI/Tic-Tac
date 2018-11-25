5c84a321656768de9142d8bea609299c6b799a00
Leron Julian <leronjulian@hotmail.com>
2018-11-25 15:18:26 -0500

    Changed dict value to a list instead of a string. Added testing function

--- optimalPosition.py
+++ optimalPosition.py
@@ -13,13 +13,34 @@ def optimalMove(fileName):
                 line_count += 1
             else:
                 #print(f'{row[0]}\t{row[1]}\t{row[2]}\t')
-                optimalDict[f'{row[1]}'] = list(f'{row[2]}'.split("[]"))
+                temp = f'{row[2]}'
+                temp_list = [[int(temp[1])], [int(temp[4])]]
+                
+                optimalDict[f'{row[1]}'] = temp_list
                 line_count += 1
         return(optimalDict)
 
 def main():
     optimalMove('SampleBoards.csv')
 
-
+'''
+#Test to see if dictionary prints out the correct optimal position
+def test():
+    fileName = 'SampleBoards.csv'
+    with open(fileName) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:        
+                test = optimalMove('SampleBoards.csv')
+                print(test[f'{row[1]}'])
+                line_count += 1
+'''
 
 main()
+
+#test()
+
+
